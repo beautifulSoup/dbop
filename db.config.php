8896e809341db7406f86e668a56b71c72f572a85
Tang Likang <tanglikang@163.com>
2014-11-07 14:32:05 +0800

    Update db.config.php

--- db.config.php
+++ db.config.php
@@ -1,10 +1,5 @@
 <?php
-/*
-	define('HOST', '121.40.157.36');
-	define('DB', 'tasktracking');
-	define('USER', 'root');
-	define('PASSWORD', 'xiangtao@2014');
-*/
+
 
 
 	define('HOST', 'localhost');
@@ -13,13 +8,5 @@
 	define('PASSWORD', '');
 	define('CHARSET', 'utf8');
 
-
-
-/*
-	define('HOST', '192.168.1.101');
-	define('DB', 'tasktracking');
-	define('USER', 'dev3');
-	define('PASSWORD', 'xiangtao2014');
-	*/
 ?>
 
